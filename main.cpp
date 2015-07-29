a8452ee3ad1ff5b5f45f2e12c18fb665d9a77c80
Pierre Lepers <pierre.lepers@makemepulse.com>
2015-07-29 17:21:48 +0200

    basic export - inline steam - no indices

--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char* argv[])
     ValueArg<string> a_output("o","output","output binary file",true,"","string", cmd );
     
     SwitchArg p_export("p","positions", "export Position", true);
-    SwitchArg t_export("t","uvs", "export Uvs", true);
-    SwitchArg n_export("n","normals", "export Normals", true);
+    SwitchArg t_export("t","uvs", "export Uvs", false);
+    SwitchArg n_export("n","normals", "export Normals", false);
     cmd.add( p_export );
     cmd.add( t_export );
     cmd.add( n_export );
@@ -91,6 +91,8 @@ int main(int argc, char* argv[])
     }
     
     
+    
+    
 	// save
     FILE* file;
     file = fopen(output, "wb" );
@@ -106,69 +108,3 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-
-
-double getNewBase( char min, char max ) {
-    double newbaseMax = pow( pow( 2.0, (double)max ), 1.0/128.0 );
-    double newbaseMin = pow( pow( 2.0, (double)min ), -1.0/128.0 );
-    
-    if( newbaseMax > newbaseMin)
-        return newbaseMax;
-    return newbaseMin;
-}
-
-
-
-unsigned char* f32toRgbe( float* pixels, int w, int h, double base ) {
-    
-    //base = 2.0;
-    
-    int j;
-    
-    int resSize = w*h;
-    
-    unsigned char* rgbe = ( unsigned char* ) malloc( resSize*4*sizeof( unsigned char* ) );
-    
-    float r, g, b;
-    double e, re;
-    int f;
-    
-    double logbase = log( base );
-    
-    
-    int c = 0;
-    int fc = 0;
-    for (j = 0; j < resSize; j++) {
-        
-        fc = j*3;
-        c = j*4;
-        
-        r = pixels[fc];
-        g = pixels[fc+1];
-        b = pixels[fc+2];
-        
-        re = max( r, max( g, b ) );
-        
-        f = int( ceil( log( re ) / logbase ) );
-        
-        if( f < -128.0f ) f = -128.0f;
-        if( f > 127.0f ) f = 127.0f;
-        
-        e = pow( base, f );
-        
-        r = r*255.0f / e;
-        g = g*255.0f / e;
-        b = b*255.0f / e;
-        
-        f += 128.0f;
-        
-        
-        rgbe[c] = char( r );
-        rgbe[c+1] = char( g );
-        rgbe[c+2] = char( b );
-        rgbe[c+3] = char( f );
-    }
-    
-    
-    return rgbe;
-}
